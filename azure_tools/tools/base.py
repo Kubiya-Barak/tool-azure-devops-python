f45bd051653f84f0e703fbbad03ff440d81d04c8
Kubiya-Barak <barak.nagar@kubiya.ai>
2024-09-26 12:53:01 +0300

    commit

---
+++ azure_tools/tools/base.py
@@ -0,0 +1,33 @@
+from kubiya_sdk.tools.models import Tool
+from .common import COMMON_ENVIRONMENT_VARIABLES, COMMON_SECRET_VARIABLES
+
+AZURE_ICON_URL = "https://azure.microsoft.com/svghandler/azure-logo/?width=300&height=300"
+
+# View https://learn.microsoft.com/en-us/azure/devops/cli/?view=azure-devops for Azure DevOps
+# details
+SCRIPT_LOGIN = """
+az extension add --name azure-devops
+echo $AZURE_DEVOPS_PAT | az devops login --organization $AZURE_DEVOPS_ORG_URL
+az devops configure --defaults organization=$AZURE_DEVOPS_ORG_URL
+az devops configure --defaults project=$AZURE_DEVOPS_PROJECT
+"""
+
+class AzureCliTool(Tool):
+    def __init__(self, name, description, content, args, long_running=False, mermaid_diagram=None):
+        # Combine the login and received content into a single script
+        full_content = f"{SCRIPT_LOGIN}\n{content}"
+
+        super().__init__(
+            name=name,
+            description=description,
+            icon_url=AZURE_ICON_URL,
+            type="docker",
+            image="mcr.microsoft.com/azure-cli:latest",
+            content=full_content,
+            args=args,
+            env=COMMON_ENVIRONMENT_VARIABLES,
+            secrets=COMMON_SECRET_VARIABLES,
+            long_running=long_running,
+            mermaid_diagram=mermaid_diagram
+        )
+
